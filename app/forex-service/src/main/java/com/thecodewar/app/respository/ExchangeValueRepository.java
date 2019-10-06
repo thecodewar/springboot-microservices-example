dbb85c8799cf8e203d4e533c28899036e92142d5
akhalique <adarsh.khalique@gmail.com>
2019-10-07 00:29:08 +0500

    Microservices

---
+++ app/forex-service/src/main/java/com/thecodewar/app/respository/ExchangeValueRepository.java
@@ -0,0 +1,9 @@
+package com.thecodewar.app.respository;
+
+import com.thecodewar.app.entity.ExchangeValue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+
+    ExchangeValue findByFromAndTo(String from, String to);
+}
---
+++ currencyconversion/currencyconversion/src/main/java/com/thecodewar/currencyconversion/CurrencyconversionApplication.java
@@ -0,0 +1,17 @@
+package com.thecodewar.currencyconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+//@EnableFeignClients("com.thecodewar.currencyconversion")
+//@EnableDiscoveryClient
+public class CurrencyconversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyconversionApplication.class, args);
+	}
+
+}
