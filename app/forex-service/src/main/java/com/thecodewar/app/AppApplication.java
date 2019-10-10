5f26810d52dd58d716f747b16d5d188a11f34568
akhalique <adarsh.khalique@gmail.com>
2019-10-10 06:13:13 +0500

    Eureka Server and client implementation.

--- app/forex-service/src/main/java/com/thecodewar/app/AppApplication.java
+++ app/forex-service/src/main/java/com/thecodewar/app/AppApplication.java
@@ -2,8 +2,10 @@ package com.thecodewar.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AppApplication {
 
 	public static void main(String[] args) {
